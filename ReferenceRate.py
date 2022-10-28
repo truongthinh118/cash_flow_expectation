10413f9c5241047ce06a96bf5785d22d0663aaec
truongthinh118 <truongthinh118001@gmail.com>
2022-10-28 17:02:06 +0700

    commit

--- ReferenceRate.py
+++ ReferenceRate.py
@@ -45,54 +45,6 @@ def render_refernce_page(service):
 
 def render_reference_saving():
     render_refernce_page('Saving')
-    # bank_list = (deposit_rate['Bank'])
-    # expander = st.expander('Reference Saving Rate Detail')
-    # render_expander(expander)
-    # col1,col2 = st.columns(2)
-    # bank = col1.selectbox("Choose The Bank", (bank_list))
-    # selected_bank=[bank]
-    # col2.write("\n")
-    # col2.write("\n")
-    # # onclick = col2.button("+")
-    # container = col2.container().empty()
-
-    # if st.session_state.get('button1') != True:
-    #     st.session_state['button1'] = container.button("+")
-
-    # if st.session_state['button1']:
-    #     container.empty()
-    #     bank2 = col1.selectbox("",(bank_list.drop(deposit_rate.loc[bank_list==bank].index,axis=0,inplace=False)),label_visibility='collapsed')
-    #     selected_bank.append(bank2)
-    # reference_rate = get_reference_rate('Saving Rate', selected_bank)
-
-    # fvtab, pvtab = st.tabs(["Expect Cash Flow", "Saving Goal"])
-    # with fvtab:
-    #     form = st.form("fvform")
-    #     duration = form.slider("Duration - Month", 1, 36)
-    #     rate = get_rate('Saving Rate',reference_rate,duration)
-    #     pv = form.number_input("Input Amount You Saving", step=100)
-    #     pmt = form.number_input("Payment", step=100)
-    #     submitted2 = form.form_submit_button(label="Submit")
-    #     if (submitted2):
-    #         string = "Deposit rate of {bank} in {duration} is: {rate}%"
-    #         st.caption(string.format(
-    #             bank=bank, duration=duration, rate=rate))
-    #         rate = float(rate.replace(",", "."))/100/12
-    #         expect_fv(rate=rate, duration=duration, pv=pv, pmt=pmt)
-
-    # with pvtab:
-    #     form = st.form("pvform")
-    #     duration = form.slider("Duration - Month", 1, 36)
-    #     rate = get_rate('Saving Rate',reference_rate,duration)
-    #     fv = form.number_input("Input Your Saving Goal", step=100)
-    #     pmt = form.number_input("Payment", step=100)
-    #     submitted2 = form.form_submit_button(label="Submit")
-    #     if (submitted2):
-    #         string = "Deposit rate of {bank} in {duration} is: {rate}%"
-    #         st.caption(string.format(
-    #             bank=bank, duration=duration, rate=rate))
-    #         rate = float(rate.replace(",", "."))/100/12
-    #         expect_pv(rate=rate, duration=duration, fv=fv, pmt=pmt)
 
 
 def render_expander(expander, df):
@@ -170,14 +122,6 @@ def get_rate(service, reference_rate, bank, period_index):
     rate = ""
     if (service == "Saving"):
         rate = reference_rate.set_index('Bank').T.iloc[period_index][bank]
-        # duration_list = [int(str(col)[0:3]) for col in reference_rate.drop(
-        #     'Bank', axis=1, inplace=False).columns]
-        # index = check_interval_value(duration_list, duration)
-        # while rate == "":
-        #         rate = reference_rate.drop('Bank', axis=1).iat[0, index]
-        #         index = index-1
-        #         if index <= -1:
-        #             break
 
     else:
         rate = reference_rate.iat[0, 1]
@@ -197,9 +141,6 @@ def check_interval_value(list, value):
 
 
 def expect_fv(expire, banks, rates, periods, pv, pmt):
-    # result = npf.fv(rate=rate, nper=duration, pv=-pv, pmt=-pmt)
-    # st.markdown("Total cash that you will be received is:  <b style=\"color:green;\">{result}</b>".format(
-    #     result="{:,}".format(result)), unsafe_allow_html=True)
     df = pd.DataFrame()
     for i in range(len(banks)):
         bank = banks[i]
